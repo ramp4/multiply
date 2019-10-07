9d2eba5365772b8dd8efc3dd050195d283b8d4ce
ramp4 <rampatv13@gmail.com>
2019-10-07 22:31:59 +0300

    feat: implement multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  first = first.split('').reverse();
+  second = second.split('').reverse();
+  let result = [];
+
+  for (i = 0; first[i] >= 0; i++) {
+    for (j = 0; second[j] >= 0; j++) {
+      if (!result[i + j]) {
+        result[i + j] = 0;
+      }
+
+      result[i + j] += first[i] * second[j];
+    }
+  }
+
+  for (i = 0; result[i] >= 0; i++) {
+    if (result[i] >= 10) {
+      if (!result[i + 1]) {
+        result[i + 1] = 0;
+      }
+
+      result[i + 1] += Math.floor(result[i] / 10);
+      result[i] = result[i] % 10;
+    }
+  }
+
+  return result.reverse().join('');
+};
